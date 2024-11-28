9cd4bd311c27a4ff95c055e0a942f40f815e4e13
n-pravab <23prabhav@gmail.com>
2024-11-29 00:31:35 +0545

    kanban

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # kanban
+
+# README FILE
+
+## Setup
+
+1.Clone the project from remote github repository
+2.Open terminal at the root directory of the project and Install necessary dependencies with `npm install` 3. Run the project with command `npm run dev` 4. Follow the link with `Local : http://localhost:<port>/` and view the project in your browser
+
+## Tech used:
+
+1. Dnd-kit : used for drag & drop functionality
+2. Vite : for fast development server and optimized production builds
+3. Tailwind-CSS : for rapid UI development with a consistent design
+
+## Limits/Trade-offs:
+
+1. Limited Error Handling
+2. Unit and Integration Tests not performed
+
+## Future Improvements:
+
+1. Improved Error Handling
+2. Unit and Integration Tests
---
+++ src/components/Card.tsx
@@ -0,0 +1,72 @@
+import { FC } from "react";
+import { CSS } from "@dnd-kit/utilities";
+import { useSortable } from "@dnd-kit/sortable";
+import { FiTrash } from "react-icons/fi";
+
+export type CardType = {
+  id: string;
+  title: string;
+};
+
+interface CardProps {
+  id: string;
+  title: string;
+  columnId: string;
+  handleDeleteCard: (columnId: string, cardId: string) => void;
+}
+
+const Card: FC<CardProps> = ({ id, title, columnId, handleDeleteCard }) => {
+  const { attributes, listeners, setNodeRef, transform } = useSortable({
+    id: id,
+  });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+  };
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this column?"
+    );
+    if (confirmed) {
+      handleDeleteCard(columnId, id);
+    }
+  };
+
+  const handleKeyDown = (
+    e: React.KeyboardEvent<HTMLButtonElement>,
+    columnId: string,
+    cardId: string
+  ) => {
+    e.stopPropagation();
+    if (e.key === "Enter" || e.key === " ") {
+      handleDeleteCard(columnId, cardId);
+    }
+  };
+
+  return (
+    <div
+      ref={setNodeRef}
+      {...attributes}
+      style={style}
+      className="m-2.5 bg-white p-2.5 rounded-lg shadow-md text-gray-800 flex items-center justify-between"
+    >
+      <div id={id} {...listeners} className="flex-grow cursor-grab">
+        <p className="font-medium">{title}</p>
+      </div>
+
+      <button
+        onClick={handleClick}
+        onKeyDown={(e) => handleKeyDown(e, columnId, id)}
+        tabIndex={0}
+        className="text-red-500 hover:text-red-700"
+        aria-label={`Delete card ${title}`}
+      >
+        <FiTrash />
+      </button>
+    </div>
+  );
+};
+
+export default Card;
